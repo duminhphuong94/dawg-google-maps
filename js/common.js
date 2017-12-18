982c0ad9c316ed3d8dffbd7aec42441c69fb03fd
sricharankrishnan <sricharan.krishnan@gmail.com>
2017-12-18 16:08:47 +0530

    Google Places API - Updates for the Day

--- js/common.js
+++ js/common.js
@@ -86,6 +86,7 @@
 
   function googlePlacesLibraryFunc() {
     if(document.location.href.indexOf("google-places-api-js") > -1) {
+      /*
       (function nearbySearchRequest1() {
         var location1 = new google.maps.LatLng(35.6895, 139.6917);
         var map1 = new google.maps.Map(document.getElementById("gMap1"), {
@@ -354,10 +355,10 @@
           }
         }
 
-        function createMarker(locationObj) {
+        function createMarker(locationobj) {
           var marker = new google.maps.Marker({
             map: map8,
-            position: locationObj.geometry.location
+            position: locationobj.geometry.location
           });
         }
       })();
@@ -423,23 +424,50 @@
         var location10 = new google.maps.LatLng(36.1699, -115.1398);
         var map10 = new google.maps.Map(document.getElementById("gMap10"), {
           center: location10,
-          zoom: 13
+          zoom: 12
         });
+        
         var service = new google.maps.places.PlacesService(map10);
         service.textSearch({
           query: "Pawn Shops in Las Vegas, NV"
         }, resultCallback);
 
+        var pageNumber = 1;
         function resultCallback(result, status, pagination) {
           if(status === google.maps.places.PlacesServiceStatus.OK) {
             console.log(result);
             console.log(pagination);
-            var listParent = $("#gMap10").parent().find(".objectResultList");
+            var toBeClonedParent = $("#paginationClone");
+            var cloneParent = toBeClonedParent.clone();
+            cloneParent.find(".headingSegment").text("Page Number: " + pageNumber);
             Array.prototype.forEach.call(result, function(thisObj, index) {
               createMarker(thisObj);
-              var liElement = $("<li></li>").html("<b>Establishment No " + index + ":</b> " + thisObj.name + " | " + thisObj.formatted_address);
-              liElement.appendTo(listParent);
+              var borderBox = $("#borderBoxClone").clone();
+              borderBox.find(".headerIcon").attr("src", thisObj.icon);
+              borderBox.find(".establishmentName").text(thisObj.name);
+              var openStatusEl = borderBox.find(".openStatus");
+              if(!thisObj.opening_hours.open_now) {
+                openStatusEl.text("Open")
+              }
+              else {
+                openStatusEl.addClass("closedStatus").text("Closed");
+              }
+              borderBox.find(".latlngValue").text(Number.prototype.toPrecision.call(thisObj.geometry.location.lat(), 4) + " | " + Number.prototype.toPrecision.call(thisObj.geometry.location.lng(), 4));
+              var categoryElement = borderBox.find(".typesValue");
+              var catString = "";
+              Array.prototype.forEach.call(thisObj.types, function(typeString, q) {
+                catString += (catString === "") ? typeString : ", " + typeString;
+              });
+              categoryElement.text(catString);
+              borderBox.find(".ratingValue").text(thisObj.rating);
+              borderBox.removeAttr("id").removeClass("hide");
+              borderBox.appendTo(cloneParent.find(".bodySegment"));
+              if((index + 1) % 2 === 0) {
+                $('<div class="clearfix hidden-xs"></div>').appendTo(cloneParent.find(".bodySegment"));
+              }
             });
+            cloneParent.removeClass("hide").removeAttr("id").appendTo($("#gMap10").parent());
+            pageNumber += 1;
             if(pagination.hasNextPage) {
               $("#gMap10").parent().find(".loadMoreButton").on("click", function() {
                 console.log("next page");
@@ -460,6 +488,52 @@
           var marker = new google.maps.Marker({
             map: map10,
             position: locationObj.geometry.location
+          });
+        }
+      })();
+      */
+
+      (function placesDetailHandling() {
+        var location11 = new google.maps.LatLng(19.0760, 72.8777);
+        var map11 = new google.maps.Map(document.getElementById("gMap11"), {
+          center: location11,
+          zoom: 12
+        });
+        var requestObject = {
+          query: "Taj Mahal Palace and Towers hotel, Mumbai",
+        };
+        var service = new google.maps.places.PlacesService(map11);
+        service.textSearch(requestObject, requestCallBack);
+        
+        function requestCallBack(resultArray, status) {
+          if(status === google.maps.places.PlacesServiceStatus.OK) {
+            console.log(resultArray);
+            map11.setZoom(18);
+            map11.setCenter(resultArray[0].geometry.location);
+            Array.prototype.forEach.call(resultArray, function(locationObj, index) {
+              service.getDetails({
+                placeId: locationObj.place_id
+              }, processGooglePlaceId);
+            });
+          }
+          else {
+            console.error("Service Request Error, Status => ", status); 
+          }
+        }
+
+        function processGooglePlaceId(result, status) {
+          if(status === google.maps.places.PlacesServiceStatus.OK) {
+            console.log(result);
+          }
+          else {
+            console.error("Place ID Request Error => ", status);
+          }
+        }
+        
+        function createMarker(locationobj) {
+          var marker = new google.maps.Marker({
+            map: map11,
+            position: locationobj.geometry.location
           });
         }
       })();
